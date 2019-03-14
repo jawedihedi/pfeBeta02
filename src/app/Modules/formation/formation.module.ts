8250f0a0c3617765f77bafb6d310e50700f7e121
mariemhajjem <mariemhajjem10@gmail.com>
2019-03-14 13:23:56 +0100

    front

---
+++ src/app/Modules/formation/formation.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormationRoutingModule } from './formation-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormationRoutingModule
+  ]
+})
+export class FormationModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { Page404Component } from './core/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule
