664073290702cdd00b51d845e71f70e1b840fb23
mariemhajjem <mariemhajjem10@gmail.com>
2019-03-14 13:29:43 +0100

    formCRUD

--- src/app/Modules/formation/formation.module.ts
+++ src/app/Modules/formation/formation.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormationRoutingModule } from './formation-routing.module';
+import { CRUDComponent } from './crud/crud.component';
+import { FormListComponent } from './form-list/form-list.component';
+import { FormItemComponent } from './form-item/form-item.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CRUDComponent, FormListComponent, FormItemComponent],
   imports: [
     CommonModule,
     FormationRoutingModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { Page404Component } from './core/page404/page404.component';
+import { CRUDComponent } from './formation/crud/crud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    Page404Component
+    Page404Component,
+    CRUDComponent
   ],
   imports: [
     BrowserModule
