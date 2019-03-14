9a57463aa8e31694e123a9a4f1f7cdad8a87f662
mariemhajjem <mariemhajjem10@gmail.com>
2019-03-14 21:13:26 +0100

    userPass

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,66 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const app = express();
-const mongoose = require("mongoose");
-config = require('./db');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
- 
-app.listen(3000, ()=> console.log("Listenig on port 3000"));
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
+config = require('./db');
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Initiate our app
+const app = express();
+
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if(!isProduction) {
+  app.use(errorHandler());
+}
+
+
+//Configure Mongoose
+mongoose.connect(config.DB,{ useNewUrlParser: true });
+mongoose.set('debug', true);
+
+//MOdels routes
+require('./Models/Users');
+require('./config/passport');
+app.use(require('./routes'));
+
+//Error handlers & middlewares
+if(!isProduction) {
+    app.use((err, req, res) => {
+      res.status(err.status || 500);
+  
+      res.json({
+        errors: {
+          message: err.message,
+          error: err,
+        },
+      });
+    });
+  }
+  
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+  
+    res.json({
+      errors: {
+        message: err.message,
+        error: {},
+      },
+    });
+  });
+  
+  app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { Page404Component } from './core/page404/page404.component';
-import { CRUDComponent } from './formation/crud/crud.component';
+ 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    Page404Component,
-    CRUDComponent
+    Page404Component 
   ],
   imports: [
     BrowserModule
